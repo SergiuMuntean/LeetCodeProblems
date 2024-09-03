230f446557d68c83913bf744619f3c2fcd15ed44
Sergiu Muntean <111748157+SergiuMuntean@users.noreply.github.com>
2024-09-03 18:22:39 +0300

    Update global-and-local-inversions.cpp

--- Math/global-and-local-inversions.cpp
+++ Math/global-and-local-inversions.cpp
@@ -4,7 +4,6 @@ public:
         if(nums.size() <= 2) {
             return true;
         }
-        int globalInversions{0};
         int maxi{-1};
         for(int i{0}; i < nums.size() - 2; i++) {
             maxi = max(nums.at(i), maxi);
