0d28b7446e0cf32491ae650980f3a36cf1d9234c
Sergiu Muntean <111748157+SergiuMuntean@users.noreply.github.com>
2024-09-26 17:38:38 +0300

    Create binary-tree-inorder-traversal.c

---
+++ Trees/binary-tree-inorder-traversal.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+void helper(struct TreeNode* root, int** arr, int* size) {
+    if(root == NULL) {
+        return;
+    }
+    helper(root->left, arr, size);
+    *arr = (int*) realloc(*arr, ((*size) + 1) * sizeof(int));
+    (*arr)[*size] = root->val;
+    (*size)++;
+    helper(root->right, arr, size);
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* arr = (int*) malloc(0 * sizeof(int));
+    *returnSize = 0;
+    helper(root, &arr, returnSize);
+    return arr;
+}
